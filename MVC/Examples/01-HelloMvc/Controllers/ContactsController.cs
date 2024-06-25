e197baae24d7639210730a16c4890e279ab2f863
SkillFoundryIO <eswise@gmail.com>
2024-06-25 10:02:46 -0400

    Example projects for MVC videos

---
+++ MVC/Examples/01-HelloMvc/Controllers/ContactsController.cs
@@ -0,0 +1,20 @@
+﻿using HelloMvc.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloMvc.Controllers
+{
+    public class ContactsController : Controller
+    {
+        public IActionResult List()
+        {
+            var model = new List<Contact>
+            {
+                new Contact { ContactID=1, Name="Joe Schmoe", Email="jschmoe@example.com"},
+                new Contact { ContactID=2, Name="Jane Doe", Email="jdoe@example.com"},
+                new Contact { ContactID=3, Name="Dwayne Robinson", Email="drobinson@example.com"},
+            };
+
+            return View(model);
+        }
+    }
+}
