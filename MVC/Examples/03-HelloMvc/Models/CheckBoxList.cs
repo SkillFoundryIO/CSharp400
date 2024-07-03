e5602d02e8c554bbf725205065a6062b62a5b5e9
SkillFoundryIO <eswise@gmail.com>
2024-07-03 13:26:09 -0400

    Part 3 examples.

---
+++ MVC/Examples/03-HelloMvc/Models/CheckBoxList.cs
@@ -0,0 +1,14 @@
+﻿namespace HelloMvc.Models
+{
+    public class CheckBoxList
+    {
+        public List<CheckBoxItem> SelectedCountries { get; set; }
+    }
+
+    public class CheckBoxItem
+    {
+        public string ID { get; set; }
+        public string Text { get; set; }
+        public bool Selected { get; set; }
+    }
+}
